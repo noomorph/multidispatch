98bf41390cacc1f9b70dffbe9aba28ed3ecba77b
Yaroslav Sergeyev <noomorph@gmail.com>
2013-10-02 08:03:46 +0300

    Made couple of tests cleaner

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,31 +1,39 @@
+require "spec_helper"
+
 describe "integration test" do
 
-  it "should not overwrite methods of previous classes" do
+  context "when 2 classes have method with same name" do
     class Intersect1
       include Multidispatch
-      def foo() "foo"; end
+      def foo() 1; end
     end
 
     class Intersect2
       include Multidispatch
-      def foo() "bar"; end
+      def foo() 2; end
     end
 
-    Intersect1.new.foo.should == "foo"
-  end
-
-  it "should work" do
-    class Foo
-      include Multidispatch
-
-      def foo() "foo"; end
-      def foo(a) "foo #{a}"; end
-      def foo(n, a) "#{n} foo #{a}"; end
+    it "does not overwrite previously defined method" do
+      Intersect1.new.foo.should == 1
     end
 
-    f = Foo.new
-    f.foo().should == "foo"
-    f.foo(5).should == "foo 5"
-    f.foo(0,1).should == "0 foo 1"
+    it "allows to define next method with same name" do
+      Intersect2.new.foo.should == 2
+    end
   end
+
+  class Bar
+    include Multidispatch
+
+    def foo() "foo"; end
+    def foo(a) "foo #{a}"; end
+    def foo(n, a) "#{n} foo #{a}"; end
+  end
+
+  describe Bar do
+    it { subject.foo.should == "foo" }
+    it { subject.foo(5).should == "foo 5" }
+    it { subject.foo(0,1).should == "0 foo 1" }
+  end
+
 end
