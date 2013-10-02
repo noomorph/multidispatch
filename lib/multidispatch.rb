70c66ae6d27ba6c5a9f3b114b96ad0eff8663081
Yaroslav Sergeyev <noomorph@gmail.com>
2013-10-02 08:02:58 +0300

    Multidispatch::Store is moved to separate file

--- lib/multidispatch.rb
+++ lib/multidispatch.rb
@@ -1,46 +1,7 @@
+require "multidispatch/store"
 require "multidispatch/version"
 
 module Multidispatch
-
-  class Store
-    attr_reader :store
-
-    def initialize
-      @store = {}
-      @locked = nil
-    end
-
-    def get(klass, method_name, arity)
-      class_methods = @store[klass.name] || {}
-      methods = class_methods[method_name] || {}
-      method_body = methods[-1] || methods[arity]
-      raise NoMethodError unless method_body
-      method_body
-    end
-
-    def set(klass, method_name)
-      return nil if locked?
-
-      class_methods = @store[klass.name] ||= {}
-      methods = class_methods[method_name] ||= {}
-      method = klass.instance_method(method_name)
-      methods[method.arity] = method
-    end
-
-    def locked?
-      @locked
-    end
-
-    def lock!
-      @locked = true
-      self
-    end
-
-    def release!
-      @locked = nil
-      self
-    end
-  end
 
   def self.included(base)
     base.extend(ClassMethods)
